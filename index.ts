ef960a10c1ab3ba820a02778855c5eff8dc5cd5b
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-06-04 04:57:18 +0100

    feat(Stream): add Stream type

--- index.ts
+++ index.ts
@@ -5,3 +5,37 @@ export interface Sink<T> {
 
     end(): void;
 }
+
+export class Stream<T> {
+    private prebuffer: T[] | null = [];
+    private sinks: Array<Sink<T>> | null = [];
+
+    constructor(emitter: Emitter<T>) {
+        emitter(element => {
+            if (this.prebuffer != null) {
+                this.prebuffer.push(element);
+            } else if (this.sinks != null) {
+                this.sinks.forEach(sink => sink.element(element));
+            }
+        }, () => {
+            if (this.sinks != null) {
+                this.sinks.forEach(sink => sink.end());
+            }
+
+            this.sinks = null;
+        });
+    }
+
+    public to(sink: Sink<T>): void {
+        if (this.prebuffer != null) {
+            this.prebuffer.forEach(element => sink.element(element));
+            setImmediate(() => this.prebuffer = null);
+        }
+
+        if (this.sinks == null) {
+            sink.end();
+        } else {
+            this.sinks.push(sink);
+        }
+    }
+}
